fcc52cb58ad2e4960e3bc1ca7df16857016749f6
Wildan Nursyamsi <125271650+AbiPasundan@users.noreply.github.com>
2026-01-22 09:40:58 +0700

    feat: Initialize React app with Box component

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import {createRoot} from "react-dom/client"
+
+const el = document.getElementById("root")
+const root = createRoot(el)
+
+function Box() {
+  return (<h1>Halo Dunia</h1>)
+}
+
+root.render(<Box/>)
 
